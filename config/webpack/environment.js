544b0438c2e53a04d1c2892d748268e53ea00dc8
CristinaBogdanoff <cristinabogdanoff55@gmail.com>
2024-08-21 08:27:19 +1000

    WHY MISTAKES

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const path = require('path');
-
-// Add this to explicitly set the entry point
-environment.config.set('entry', {
-   application: path.resolve(__dirname, '../../app/javascript/application.js')
-});
-
-module.exports = environment;
+module.exports = environment
